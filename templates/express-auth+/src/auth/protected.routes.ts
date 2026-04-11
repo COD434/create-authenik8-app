7c18b981d20268442acf4670d81f7f96c2a3d394
COD434 <seeisakarabo2@gmail.com>
2026-04-11 23:02:45 +0200

    upgrade:smootherux

--- templates/express-auth+/src/auth/protected.routes.ts
+++ templates/express-auth+/src/auth/protected.routes.ts
@@ -2,9 +2,12 @@ import express from "express";
 import { getAuth } from "./auth";
 
 const router = express.Router();
-const auth = getAuth();
+
 
 router.get("/protected", auth.requireAdmin, (req, res) => {
+	const auth = getAuth();
+  return auth.requireAdmin(req, res, next);
+}, (req, res) => {
   res.json({ message: "Protected route" });
 });
 
