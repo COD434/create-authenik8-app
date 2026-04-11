b037954934e3278eb3b0dcc9423317ef43a26b71
COD434 <seeisakarabo2@gmail.com>
2026-04-11 23:37:58 +0200

    fix:fixed undefined variable

--- templates/express-auth+/src/auth/protected.routes.ts
+++ templates/express-auth+/src/auth/protected.routes.ts
@@ -1,11 +1,14 @@
 import express from "express";
 import { getAuth } from "./auth";
 
+
 const router = express.Router();
 
 
-router.get("/protected", auth.requireAdmin, (req, res) => {
-	const auth = getAuth();
+router.get("/protected", (req, res) => {
+
+	const auth = getAuth()
+
   return auth.requireAdmin(req, res, next);
 }, (req, res) => {
   res.json({ message: "Protected route" });
